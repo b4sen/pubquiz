ed5aab17144036525d88d07bf16d3c0a8b454b3c
Kolos Niedermayer <kolos.niedermayer@gmail.com>
2022-11-09 21:13:51 +0100

    Serve bundled react frontend as static assets

--- pubquiz/server.py
+++ pubquiz/server.py
@@ -1,6 +1,20 @@
+import os
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import RedirectResponse
 
 from .api import api_router
 
+DIRNAME = os.path.dirname(os.path.abspath(__file__))
+static_dir = os.path.join(DIRNAME, 'web', 'build')
+
 app = FastAPI()
 app.include_router(api_router)
+
+
+@app.get("/")
+async def index():
+    return RedirectResponse(url="/index.html")
+
+app.mount("/", StaticFiles(directory=static_dir))
